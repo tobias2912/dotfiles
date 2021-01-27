59d0c94be4c9d6eb26cd60538af4684d16f8b9b9
Your Name <you@example.com~>
2021-01-27 13:15:28 +0100

    first

---
+++ uploadconfigs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#copy configs from default position and push to github
+cp ~/.config/kitty/kitty.conf ./kitty.conf
+cp ~/.config/ranger/rc.conf ./rc.conf
+cp ~/.config/kwinrc ./kwin.rc
+cp ~/.vimrc ./vimrc
+cp ~/.zshrc ./zshrc
+
+cp '${HOME}/.config/Code - OSS/User/keybindings.json' ./vscode/keybindings.json
+cp '${HOME}/config/Code - OSS/User/settings.json' ./vscode/settings.json
+
+git add .
+git commit -m 'configs updated by script' && git push
