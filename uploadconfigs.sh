fc67267bea5e1156ccc21e3307b3b487758914d3
Your Name <you@example.com~>
2021-02-20 11:08:15 +0100

    typo

--- uploadconfigs.sh
+++ uploadconfigs.sh
@@ -4,8 +4,9 @@
 cp ~/.config/kitty/kitty.conf ./kitty.conf
 cp ~/.config/ranger/rc.conf ./rc.conf
 cp ~/.config/kwinrc ./kwin.rc
-cp ~/.vimrc ./vimrc
-cp ~/.zshrc ./zshrc
+cp ~/.vimrc ./.vimrc
+cp ~/.zshrc ./.zshrc
+cp ~/.config/kglobalshortcutsrc ./
 
 cp "${HOME}/.config/Code - OSS/User/keybindings.json" ./vscode/keybindings.json
 cp "${HOME}/.config/Code - OSS/User/settings.json" ./vscode/settings.json
